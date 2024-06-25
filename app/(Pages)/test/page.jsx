d41d4034e9178f109e96bda0609241013a1d33d4
Rishikesh Jadhav <rishikeshjadhav21@gmail.com>
2024-06-25 13:42:03 +0530

    cards

--- app/(Pages)/test/page.jsx
+++ app/(Pages)/test/page.jsx
@@ -244,7 +244,7 @@ export default function Home() {
                 className="col-lg-4 col-md-6 wow fadeInUp"
                 data-wow-delay="0.1s"
               >
-           <div className="property-item rounded overflow-hidden" style={{boxShadow: "6px 6px 3px rgb(220, 237, 243)"}}>
+           <div className="property-item rounded overflow-hidden" style={{boxShadow: "6px 6px 3px rgb(220, 237, 243)", backgroundColor:"#FCFCFC"}}>
                   <div className="position-relative overflow-hidden">
                     <a href="">
                       <Image className="img-fluid" src={p4} alt="" />
@@ -298,7 +298,7 @@ export default function Home() {
                 className="col-lg-4 col-md-6 wow fadeInUp"
                 data-wow-delay="0.3s"
               >
-             <div className="property-item rounded overflow-hidden" style={{boxShadow: "6px 6px 3px rgb(220, 237, 243)"}}>
+             <div className="property-item rounded overflow-hidden" style={{boxShadow: "4px 4px 10px 3px lightgrey" , backgroundColor:"#FAF9F6"}}>
                   <div className="position-relative overflow-hidden">
                     <a href="">
                       <Image className="img-fluid" src={p5} alt="" />
